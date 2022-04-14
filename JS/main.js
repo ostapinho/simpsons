834b14b153a615ce158b9e095c66ee670ea71451
ostapinho <ostapinho.kap@gmail.com>
2022-04-14 22:21:34 +0200

    vikovych ostap

---
+++ JS/main.js
@@ -0,0 +1,166 @@
+// alert('hello okten')
+// document.write('hello')
+
+// let a;
+// a = 'Hello World!'
+    // console.log(a);
+
+// var a;
+// a = 'hellooktn';
+// console.log(a)
+
+// const a = 'hello'
+// console.log(a)
+
+// let a = 'open';// STRING
+
+// let bool1 = 5 > 6;
+// console.log(bool1)
+
+// let fruits = ['apple','orange','1','ok']; // МАСИВ
+// console.log(fruits)
+// console.log(fruits[1]) // orange
+
+// let user1 = {
+//     name: 'vasya',
+//     age: 31,
+//     status: true
+// };
+// console.log(user1)
+// console.log(user1.name) // vasya
+//
+// let user2 = {
+//     name: 'petya',
+//     age: 18,
+//     status: true
+// }
+// console.log(user2)
+// console.log(user2.age)
+//
+// let imya = 'anton'
+// let user3 = {
+//     name: imya,
+// }
+// console.log(user3)
+
+alert('hello')
+document.write('hello')
+let a;
+a = 'hello'
+console.log(a)
+
+alert('owu')
+document.write('owu')
+let b;
+b = 'owu'
+console.log(b)
+
+alert('com')
+document.write('com')
+let c;
+c = 'com'
+console.log(c)
+
+
+alert('ua')
+document.write('ua')
+let d;
+d = 'ua'
+console.log(d)
+
+
+alert('1')
+document.write('1')
+let e;
+e = '1'
+console.log(e)
+
+alert('10')
+document.write('10')
+let q;
+q = '10'
+console.log(q)
+
+
+alert('-999')
+document.write('-999')
+let w;
+w = '-999'
+console.log(w)
+
+alert('123')
+document.write('123')
+let r;
+r = '123'
+console.log(r)
+
+
+alert('3.14')
+document.write('3.14')
+let t;
+t = '3.14'
+console.log(t)
+
+
+alert('2.7')
+document.write('2.7')
+let y;
+y = '2.7'
+console.log(y)
+
+
+alert('16')
+document.write('16')
+let u;
+u = '16'
+console.log(u)
+
+
+alert('true')
+document.write('true')
+let i;
+i = 'true'
+console.log(i)
+
+
+alert('falce')
+document.write('falce')
+let o;
+o = 'falce'
+console.log(o)
+
+
+let book = {
+    name: 'top gear',
+    page: 220,
+    genre: 'comedia'
+}
+console.log(book)
+
+let book1 = {
+    name: 'hello world',
+    page: 321,
+    genre: 'comedia',
+    authors: ['nazar', 'andriy', 'oleh', 'stepan']
+}
+console.log(book1)
+
+
+let firstName = 'Ostap'
+let middleName = 'Orestovych'
+let lastName = 'Vikovych'
+console.log(firstName + ' ' + middleName + ' ' + lastName)
+
+
+
+
+let books = {
+    name: 'Hello Okten',
+    pages: '234',
+    genre: 'comedia'
+    // authors: ['Oleh','Anton','Andryi']
+}
+
+
+
+
